48228af8ec692fb9cc8c6925fc5e9aac3b7b1b67
ukroma1992 <ukroma1992@gmail.com>
2018-12-21 21:28:32 +0200

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,11 +1,18 @@
 # encoding: utf-8
-#
-# Создадаем массив с набором достоинств
+
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-# Создадим массив с набором мастей.
-# Буби, черви, крести, пики.
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+# Создадим массив с набором мастей, вместо названия масти — иконка-символ
+suits = ["♦", "♠", "♥ ", "♣"]
 
-# Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
